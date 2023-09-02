f3a7bab7a4219b760d838ffa9a916e64f0dfc65a
goghan123 <83848070+goghan123@users.noreply.github.com>
2023-09-02 22:24:10 +1000

    Sudoku-solver has been added

--- sudoku-solver/sudoku-solver/board.js
+++ sudoku-solver/sudoku-solver/board.js
@@ -1,21 +1,5 @@
 import { sectionIndexObtainer, sectionSelector } from "./otherDomTools.js";
-import { solve } from "./solver.js";
-
-const createCell = (cellContainer, cellId) => {
-    const newInput = document.createElement('input');
-    newInput.setAttribute('type', 'text');
-    newInput.setAttribute('id', `input-${cellId}`);
-    newInput.style.width = '8vw';
-    newInput.style.height = '8vw';
-    newInput.style.textAlign = 'center';
-    newInput.style.fontSize = '5vw';
-    newInput.style.fontWeight = 'bold';
-    newInput.style.color = 'white';
-    newInput.style.borderRadius = "10px";
-    newInput.style.background = '#11282e';
-    newInput.style.borderWidth = "0";
-    cellContainer.appendChild(newInput);
-}
+import { solve, clear } from "./solver.js";
 
 const createSampleLines = () => {
     const newSeriesSeries = [, , 8, , 5, , , , , , 2, , 1, , 8, , 9, , 4, , , , , , 6, , , , , , , , 7, , , , , 8, , 3, , 9, , 1, , , , 2, , , , , , 8, , , , , 2, , , 5, , , , 6, 5, , 3, 9, , , , 3, , , 7, , , ,];
@@ -29,18 +13,70 @@ const createSampleLines = () => {
 }
 
 export const generalHTML = () => {
+
     document.body.style.display = 'flex';
     document.body.style.flexDirection = 'column';
     document.body.style.justifyContent = 'center';
     document.body.style.backgroundColor = 'black';
-    document.body.style.height = '100vh';
+    document.body.style.height = '100%';
     document.body.style.width = '98%';
     const board = document.createElement('table');
+    //board.firstChild.style.width = '100%';
     board.setAttribute('id', 'board');
     board.style.display = 'flex';
+    //board.style.width = '84vw';
+    //board.style.height = '84vw';
+
     board.style.justifyContent = 'center';
     board.style.alignSelf = 'center';
     board.style.alignItems = 'end';
+    //board.style.height = '40vh';
+    //board.style.width = '90vw';
+
+    //board.style.marginRight = '5vw';
+    //board.style.marginLeft = '5vw';
+    //board.style.marginTop = '5vw';
+    // board.style.marginBottom = '5vw';
+    //board.style.aspectRatio = '1 / 1';
+    //const boardBody = document.getElementsByTagName('tbody');
+
+
+
+    const createCell = (cellContainer, cellId) => {
+        const newInput = document.createElement('input');
+        newInput.setAttribute('type', 'text');
+        newInput.setAttribute('id', `input-${cellId}`);
+        /*
+        newInput.style.width = '10vw';
+        newInput.style.maxWidth = '50px';
+        newInput.style.minWidth = '30px';
+
+        newInput.style.height = '10vw';
+        newInput.style.maxHeight = '50px';
+        newInput.style.minHeight = '30px';
+*/
+        //newInput.style.width = '100%';
+        //newInput.style.height = '100%';
+
+        newInput.style.width = '9vw';
+        newInput.style.height = '9vh';
+        newInput.style.maxHeight = '80px';
+        newInput.style.minHeight = '40px';
+
+
+
+        newInput.style.fontSize = '3vh';
+        newInput.style.textAlign = 'center';
+        newInput.style.fontWeight = 'bold';
+        newInput.style.color = 'white';
+        newInput.style.borderRadius = "10px";
+        newInput.style.background = '#11282e';
+        newInput.style.borderWidth = "0";
+
+       // newInput.style.aspectRatio = "1 / 1";
+
+        cellContainer.appendChild(newInput);
+    }
 
     let cellId = 0;
     for (let i = 0; i < 9; i++) {
@@ -50,6 +86,7 @@ export const generalHTML = () => {
             const newCol = newRow.insertCell(e);
             newCol.setAttribute('class', `col-${e}`);
             newCol.style.padding = "1px";
+            newCol.style.aspectRatio = "1 / 1";
             createCell(newCol, cellId);
             cellId++;
         }
@@ -85,16 +122,34 @@ export const buttons = () => {
     mainButton.setAttribute('id', 'main-button');
     mainButton.innerHTML = 'Solve it';
     mainButton.style.color = 'white';
-    mainButton.style.fontSize = '5vw';
+    mainButton.style.fontSize = '3vh';
     mainButton.style.background = '#11282e';
-    mainButton.style.height = '60px';
+    mainButton.style.height = 'fit-content';
+    mainButton.style.minHeight = '40px';
     mainButton.style.paddingRight = '20px';
     mainButton.style.paddingLeft = '20px';
     mainButton.style.border = '0';
-    mainButton.style.borderRadius = '3vw';
+    mainButton.style.borderRadius = '1vw';
     mainButton.style.marginBottom = '10px';
     mainButton.onclick = () => solve();
 
+    const clearButton = document.createElement('button');
+    clearButton.setAttribute('id', 'clear-button');
+    clearButton.innerHTML = 'Clear all';
+    clearButton.style.color = 'white';
+    clearButton.style.fontSize = '3vh';
+    clearButton.style.background = '#11282e';
+    clearButton.style.height = 'fit-content';
+    clearButton.style.minHeight = '40px';
+    clearButton.style.paddingRight = '20px';
+    clearButton.style.paddingLeft = '20px';
+    clearButton.style.border = '0';
+    clearButton.style.borderRadius = '1vw';
+    clearButton.style.marginBottom = '10px';
+    clearButton.style.marginLeft = '10px';
+    clearButton.onclick = () => clear();
+
     document.body.appendChild(buttonContainer);
     buttonContainer.appendChild(mainButton);
+    buttonContainer.appendChild(clearButton);
 }
